43eb2e6397d8a7edeba137c4f94187eaae87c5fd
Lxxyx <841380530@qq.com>
2016-03-19 08:35:13 +0800

    完善测试用例

--- test/lib.test.js
+++ test/lib.test.js
@@ -2,13 +2,13 @@ import { booksInfo } from './../src/lib/lib'
 var expect = require('chai').expect;
 
 describe('图书馆模块测试', function () {
-  it('查找图书的程序应该存在', function () {
+  it('查找图书的程序存在', function () {
     expect(booksInfo).to.exist;
   });
-  it('查找图书的程序应该是个函数', function () {
+  it('查找图书的程序是个函数', function () {
     expect(booksInfo).to.be.instanceOf(Function)
   });
-  it('返回结果应该是个数组', function () {
+  it('返回结果是个数组', function () {
     booksInfo('./lib.test.json')
       .then(data => {
         expect(data).to.be.instanceOf(Array);
