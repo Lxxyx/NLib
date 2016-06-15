22bf196a81d8ee8d17fe32e9d2da72d178538595
Lxxyx <841380530@qq.com>
2016-06-15 13:43:52 +0800

    use eslint

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,11 +3,11 @@ const cheerio = require('cheerio')
 
 const isBorrow = function (state) {
   if (!state.data && state.children) {
-    if (state.children[0].data === "可借") {
+    if (state.children[0].data === '可借') {
       return 1
     }
     return 0
-  } else if (state.data === "新书：正在上架") {
+  } else if (state.data === '新书：正在上架') {
     return 1
   }
   return 0
@@ -35,36 +35,8 @@ const getLocation = function (item) {
   return
 }
 
-const getMailContent = function (data) {
-  var content = ""
-  content += "<!DOCTYPE html>"
-  content += "<html lang=\"zh-cn\">"
-  content += "<head>"
-  content += "  <meta charset=\"UTF-8\">"
-  content += "  <title>Document<\/title>"
-  content += "  <style type=\"text\/css\">"
-  content += "    span {"
-  content += "      display:inline-block"
-  content += "      padding: 10px"
-  content += "    }"
-  content += "  <\/style>"
-  content += "<\/head>"
-  content += "<body>"
-  for (let x in data) {
-    content += '<p>' +
-      '<span>' + '书籍名称：' + data[x].title + '</span>' + '</br>' +
-      '<span>' + '书籍位置：' + data[x].location + '</span>' +
-      '<span>' + '可借阅数量：' + data[x].canBorrow + '</span>' +
-      '</p>' + '</hr>'
-  }
-  content += "<\/body>"
-  content += "<\/html>"
-  return content
-}
-
 module.exports = {
   isBorrow,
   getTitle,
-  getLocation,
-  getMailContent
+  getLocation
 }
